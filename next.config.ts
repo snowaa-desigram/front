77d5217e30ff96888c7176af8ed5ffe813521da7
Ярослав Селютин <selutin@rasa.pro>
2026-09-04 20:00:28 +1000

    chore: compact prettier and eslint

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next'
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  transpilePackages: ["three"],
-};
+  output: 'standalone',
+  transpilePackages: ['three']
+}
 
-export default nextConfig;
+export default nextConfig
